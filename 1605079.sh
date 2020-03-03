af9276ca334d29b940be7a09bd5ecea9cdade43e
Akil <cyclosis47@gmail.com>
2020-03-03 20:32:10 +0600

    copying done perfectly

--- 1605079.sh
+++ 1605079.sh
@@ -167,12 +167,283 @@ look_up_directories(){
         cd .. # for coming back from the recursion
 }
 
-look_up_directories $working_dir
+#look_up_directories $working_dir
+
+
+browse_dir(){
+
+        for dir in $1/*
+            do
+                if [ -d "$dir" ];then
+                    echo $dir is a directory 
+                    look_up_directories $dir #recursive call
+                elif [ -f "$dir" ];then
+                    #do processing of the files and copy the files to the output_dir
+                    #firstly check if the file is a text file or not, if it is a text file
+                    #extract the string and process the text file
+                    #echo $dir is a file
+                    echo beforer checking $dir
+                    if file "$dir" | grep -qEi -- 'ASCII|Unicode'; then 
+                        echo $dir is an ASCII file
+                        echo $dir
+                        # now check for the pattern of the file
+                        
+                        #calculating the total line no first
+                        total_line_no=`cat "$dir" | wc -l`
+                        echo $((total_line_no))
+
+                        if [ $start_from = "begin" ]; then 
+                            echo from begin
+                            if head -n $lines_to_look $dir | grep -qi $word_to_look; then
+                                echo found in $dir
+
+                                ((total_matched_file = total_matched_file + 1))
+                                
+                                #finding the line no from the beginning---------------------------------
+                                found_in=`grep -ni $word_to_look $dir | cut -d':' -f 1 | head -n 1`
+
+                                #modifying the file name------------------------------------------------
+
+                                file_name=`realpath $dir`
+                                #echo $file_name
+                                extension="${file_name##*.}"
+
+                                if echo $extension | grep -q \/; then 
+                                    #echo no extension
+                                    #echo $file_name
+                                    #echo woext $file_name
+
+                                    mod_f_name="${file_name//\//.}"
+                                    #echo $mod_f_name
+
+                                    new_f_name=${mod_f_name}${found_in}
+                                    #echo $new_f_name 
+                                else 
+                                    #echo extension is $extension
+
+                                    file_name_woext="${file_name%.*}"
+                                    #echo woext $file_name_woext
+
+                                    mod_f_name="${file_name_woext//\//.}"
+                                    #echo $mod_f_name
+
+                                    new_f_name=${mod_f_name}${found_in}.${extension}
+                                    #echo $new_f_name 
+                                fi
+
+                                #writing to the directory-------------------------------------------------
+                                #cp $dir "$output_dir/${new_f_name}"
+                                
+                                echo found in line no: $found_in 
+                            else 
+                                echo 
+                            fi 
+                        elif [ $start_from = "end" ]; then 
+                            echo from end
+                            if tail -n $lines_to_look $dir | grep -qi $word_to_look; then
+                                echo found in $dir
+
+                                ((total_matched_file = total_matched_file + 1))
+
+                                found_in=`grep -ni $word_to_look $dir | cut -d':' -f 1 | tail -n 1`
+
+                                #new_name=$((found_from_back)).pappa
+
+                                #echo found in $((found_from_back)).pappa
+                                #echo $new_name
+                                
+
+                                #actual_line_no=$((total_line_no - lines_to_look + found_from_back))
+
+                                #echo found on line no $actual_line_no
+
+                                #modifying the file name------------------------------------------------
+
+                                file_name=`realpath $dir`
+                                #echo $file_name
+                                extension="${file_name##*.}"
+
+                                if echo $extension | grep -q \/; then 
+                                    #echo no extension
+                                    #echo $file_name
+                                    #echo woext $file_name
+
+                                    mod_f_name="${file_name//\//.}"
+                                    #echo $mod_f_name
+
+                                    new_f_name=${mod_f_name}${found_in}
+                                    #echo $new_f_name 
+                                else 
+                                    #echo extension is $extension
+
+                                    file_name_woext="${file_name%.*}"
+                                    #echo woext $file_name_woext
+
+                                    mod_f_name="${file_name_woext//\//.}"
+                                    #echo $mod_f_name
+
+                                    new_f_name=${mod_f_name}${found_in}.${extension}
+                                    #echo $new_f_name 
+                                fi
+
+                                #writing to the directory-------------------------------------------------
+                                #cp $dir "$output_dir/${new_f_name}"
+
+                            else 
+                                echo not found
+                            fi
+
+                        
+                        fi
+
+                    fi 
+                fi
+            done
+
+}
 
 
 
 
 
+
+f(){
+    for dir in "$1"/*
+    do
+
+        if [ -d "$dir" ]; then 
+            #echo $dir 
+            #echo ekhane
+            f "$dir" 
+            #echo here
+        elif [ -f "$dir" ]; then 
+                #echo $dir 
+
+             if file "$dir" | grep -qEi -- 'ASCII|Unicode'; then 
+                        #echo $dir is an ASCII file
+                        #echo $dir
+
+                if [ $start_from = "begin" ]; then 
+                            #echo from begin
+                            if head -n $lines_to_look "$dir" | grep -qi $word_to_look; then
+                                echo found in $dir
+
+                                ((total_matched_file = total_matched_file + 1))
+                                
+                                #finding the line no from the beginning---------------------------------
+                                found_in=`grep -ni $word_to_look "$dir" | cut -d':' -f 1 | head -n 1`
+
+                                #modifying the file name------------------------------------------------
+
+                                
+                                #echo $file_name
+                                extension="${dir##*.}"
+
+                                if echo $extension | grep -q \/; then 
+                                    echo no extension
+                                    echo $dir
+                                    #echo $file_name
+                                    #echo woext $file_name
+
+                                    mod_f_name="${dir//\//.}"
+                                    #echo $mod_f_name
+
+                                    new_f_name=${mod_f_name}${found_in}
+                                    echo $new_f_name 
+                                else 
+                                    echo extension is $extension
+
+                                    file_name_woext="${dir%.*}"
+                                    echo woext $file_name_woext
+
+                                    mod_f_name="${file_name_woext//\//.}"
+                                    echo $mod_f_name
+
+                                    new_f_name=${mod_f_name}${found_in}.${extension}
+                                    echo $new_f_name 
+                                fi
+
+                                #writing to the directory-------------------------------------------------
+                                cp $dir "$output_dir/${new_f_name}"
+                                
+                                echo found in line no: $found_in 
+                            else 
+                                echo 
+                            fi
+
+                elif [ $start_from = "end" ]; then 
+                            echo from end
+                            if tail -n $lines_to_look "$dir" | grep -qi $word_to_look; then
+                                echo found in $dir
+
+                                ((total_matched_file = total_matched_file + 1))
+
+                                found_in=`grep -ni $word_to_look "$dir" | cut -d':' -f 1 | tail -n 1`
+
+                                #new_name=$((found_from_back)).pappa
+
+                                #echo found in $((found_from_back)).pappa
+                                #echo $new_name
+                                
+
+                                #actual_line_no=$((total_line_no - lines_to_look + found_from_back))
+
+                                #echo found on line no $actual_line_no
+
+                                #modifying the file name------------------------------------------------
+
+                                #echo $file_name
+                                extension="${dir##*.}"
+
+                                if echo $extension | grep -q \/; then 
+                                    #echo no extension
+                                    #echo $file_name
+                                    #echo woext $file_name
+
+                                    mod_f_name="${dir//\//.}"
+                                    #echo $mod_f_name
+
+                                    new_f_name=${mod_f_name}${found_in}
+                                    #echo $new_f_name 
+                                else 
+                                    #echo extension is $extension
+
+                                    file_name_woext="${dir%.*}"
+                                    #echo woext $file_name_woext
+
+                                    mod_f_name="${file_name_woext//\//.}"
+                                    #echo $mod_f_name
+
+                                    new_f_name=${mod_f_name}${found_in}.${extension}
+                                    #echo $new_f_name 
+                                fi
+
+                                #writing to the directory-------------------------------------------------
+                                #cp $dir "$output_dir/${new_f_name}"
+
+                            else 
+                                echo not found
+                            fi
+
+                        
+                        fi
+
+
+                
+                fi
+
+            
+            fi
+
+
+    done    
+}
+
+f $working_dir
+
+
+
+#browse_dir $working_dir
 
 
 
